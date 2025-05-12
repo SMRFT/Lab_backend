4a6718bf25b77d5ea9b72331aad641897bcdbc03
SMRFT <parthibansmrft@gmail.com>
2025-05-12 17:27:28 +0530

    update backend views

--- labbackend/models.py
+++ labbackend/models.py
@@ -124,7 +124,7 @@ class SampleCollector(AuditModel):
     name = models.CharField(max_length=255, blank=True, null=True)
     gender  = models.CharField(max_length=255, blank=True, null=True)
     phone  = models.CharField(max_length=255, blank=True, null=True)
-    email = models.EmailField()
+    email = models.CharField(max_length=255, blank=True, null=True)
     def __str__(self):
         return f"{self.name}"
 
