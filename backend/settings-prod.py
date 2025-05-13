dd4e8a34ca5f8a5110836fffa74490ae86a0c11e
SMRFT <110091550+SMRFT@users.noreply.github.com>
2025-05-13 10:52:30 +0530

    Update settings-prod.py

--- backend/settings-prod.py
+++ backend/settings-prod.py
@@ -39,7 +39,8 @@ ALLOWED_HOSTS = [
     "3.109.122.211",
     "lab.shinovadatabase.in",
     "node221967-env-5683222.in1.cloudlets.co.in",
-    'env-5683222.in1.cloudlets.co.in'
+    'env-5683222.in1.cloudlets.co.in',
+    'shinova.in1.cloudlets.co.in'
 ]
 
 X_FRAME_OPTIONS = 'ALLOWALL'
