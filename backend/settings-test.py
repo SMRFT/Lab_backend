9b52171a48d73a1368d96e9aa9c250d7b1acec20
SMRFT <parthibansmrft@gmail.com>
2025-05-13 09:45:20 +0530

    update backend views

--- backend/settings-test.py
+++ backend/settings-test.py
@@ -158,10 +158,11 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-EMAIL_BACKEND = os.getenv('EMAIL_BACKEND')
-EMAIL_HOST = os.getenv('EMAIL_HOST')
-EMAIL_PORT = int(os.getenv('EMAIL_PORT'))
-EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS') == 'True'
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'resultssmrft@gmail.com'
+EMAIL_HOST_PASSWORD = 'nfkk krtt htoq asge'  # Consider using env vars for security
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
