bab5b043480bd56380f4afd02d11aaaafa6dc227
SMRFT <parthibansmrft@gmail.com>
2025-05-26 15:37:45 +0530

    updating the invoice

--- labbackend/models.py
+++ labbackend/models.py
@@ -121,10 +121,10 @@ class RefBy(AuditModel):
     
     
 class SampleCollector(AuditModel):
-    name = models.CharField(max_length=255, blank=True, null=True)
+    name = models.CharField(max_length=255)
     gender  = models.CharField(max_length=255, blank=True, null=True)
     phone  = models.CharField(max_length=255, blank=True, null=True)
-    email = models.CharField(max_length=255, blank=True, null=True)
+    email =models.CharField(max_length=255, blank=True, null=True)
     def __str__(self):
         return f"{self.name}"
 
