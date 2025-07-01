e14c8bd350b0f9a743f2c9059e030b4733759760
SMRFT <110091550+SMRFT@users.noreply.github.com>
2025-07-01 21:56:26 +0530

    Update refundandcancellation.py

--- labbackend/Views/refundandcancellation.py
+++ labbackend/Views/refundandcancellation.py
@@ -155,7 +155,7 @@ def verify_and_process_refund(request):
 
             # Connect to MongoDB
             password = quote_plus('Smrft@2024')
-            client = MongoClient(os.getenv('LAB_DB_HOST'))
+            client = MongoClient(os.getenv('GLOBAL_DB_HOST'))
             db = client.Lab
             patients_collection = db["labbackend_patient"]
 
@@ -337,7 +337,7 @@ def verify_and_process_cancellation(request):
 
             # Connect to MongoDB
             password = quote_plus('Smrft@2024')
-            client = MongoClient(os.getenv('LAB_DB_HOST'))
+            client = MongoClient(os.getenv('GLOBAL_DB_HOST'))
             db = client.Lab
             patients_collection = db["labbackend_patient"]
 
